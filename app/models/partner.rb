89851eeeb62465658a04366c44def171d0a02e71
JimOluwaseyi <heysey128@gmail.com>
2024-11-24 17:24:23 +0100

    Add the requirement on the table

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,2 +1,4 @@
 class Partner < ApplicationRecord
-end
+    validates :first_name, :last_name, :email, :organ_name, :address, presence: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  end
