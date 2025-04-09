ce332729d57c7d03cb779d9b4b8e1add094c75f5
JimOluwaseyi <heysey128@gmail.com>
2025-04-09 12:58:40 +0100

    count for blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,10 @@
 class Blog < ApplicationRecord
   belongs_to :author
   has_one_attached :blog_image
-  validates :title, :content, :author_id, :blog_image, :is_important, presence: true
+  validates :title, :content, :excerpt, :author_id, :blog_image,  presence: true
+  validates :status, inclusion: { in: %w[draft published] }
+
+  scope :published, -> { where(status: 'published') }
+  scope :draft, -> { where(status: 'draft') }
 
 end
