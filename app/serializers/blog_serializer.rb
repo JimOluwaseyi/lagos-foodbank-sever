745ba2add881b9a2015d88acbf381df5216ea5a6
JimOluwaseyi <heysey128@gmail.com>
2025-04-09 15:14:24 +0100

    Modify the blog image url

--- app/serializers/blog_serializer.rb
+++ app/serializers/blog_serializer.rb
@@ -1,17 +1,13 @@
-# 
-
-
-
 class BlogSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  belongs_to :author, serializer: AuthorSerializer
-  attributes :id, :title, :content, :author_id, :is_important, :created_at
+belongs_to :author, serializer: AuthorSerializer
+  attributes :id, :title, :content, :excerpt, :author_id, :is_important, :created_at, :status
+  
 
-  attribute :blog_image_url
-
-  def blog_image_url
-    polymorphic_url(object.blog_image) if object.blog_image.attached?
+  attribute :blog_image
+  
+  def blog_image
+    rails_blob_url(object.blog_image, only_path: false) if object.blog_image.attached?
   end
-  
-  
 end
+
