83f770b91fbcd559454904554fb560af0433df8c
JimOluwaseyi <heysey128@gmail.com>
2025-04-09 11:22:28 +0100

    Added the name requirement

--- app/serializers/author_serializer.rb
+++ app/serializers/author_serializer.rb
@@ -1,10 +1,28 @@
+# class AuthorSerializer < ActiveModel::Serializer
+#   include Rails.application.routes.url_helpers
+#   attributes :id , :author_name , :author_image, :about_author, :facebook_url, :twitter_url, :instagram_url
+#   attribute :author_image
+#   has_many :blogs
+
+#   def author_image
+#     rails_blob_url(object.author_image, only_path: false) if object.author_image.attached?
+#   end
+# end
+
+
 class AuthorSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id , :author_name , :author_image, :about_author, :facebook_url, :twitter_url, :instagram_url
-  attribute :author_image
+  attributes :id, :author_name, :about_author, :facebook_url, :twitter_url, :instagram_url
   has_many :blogs
 
-  def author_image
-    rails_blob_url(object.author_image, only_path: false) if object.author_image.attached?
+  # Renamed to _url for clarity and using updated URL generation
+  attribute :author_image_url
+
+  def author_image_url
+    if object.author_image.attached?
+      polymorphic_url(object.author_image) # New recommended approach
+      # Or alternatively:
+      # rails_blob_url(object.author_image, disposition: "attachment")
+    end
   end
 end
--- app/serializers/blog_serializer.rb
+++ app/serializers/blog_serializer.rb
@@ -1,13 +1,17 @@
+# 
+
+
+
 class BlogSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-belongs_to :author, serializer: AuthorSerializer
+  belongs_to :author, serializer: AuthorSerializer
   attributes :id, :title, :content, :author_id, :is_important, :created_at
-  
 
-  attribute :blog_image
-  
-  def blog_image
-    rails_blob_url(object.blog_image, only_path: false) if object.blog_image.attached?
+  attribute :blog_image_url
+
+  def blog_image_url
+    polymorphic_url(object.blog_image) if object.blog_image.attached?
   end
+  
+  
 end
-
