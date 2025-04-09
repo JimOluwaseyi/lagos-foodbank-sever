baa35e5550cab7164aaf24dacbbed3c7c89f918f
JimOluwaseyi <heysey128@gmail.com>
2025-04-09 12:40:34 +0100

    blog

--- app/controllers/api/v1/blogs_controller.rb
+++ app/controllers/api/v1/blogs_controller.rb
@@ -1,9 +1,33 @@
 class  Api::V1::BlogsController < ApplicationController
-  # before_action :authenticate_admin!, only: [:create, :update, :destroy]
+  before_action :set_blog, only: [:show, :update, :destroy]
 
   # GET /blogs
+  # def index
+
+  #   per_page = params[:per_page] || 9 # Default to 9 if `per_page` is not provided
+  #   blogs = Blog.includes(:author).order(created_at: :desc).page(params[:page]).per(per_page)
+
+    
+  #   render json: {
+  #     blogs: ActiveModelSerializers::SerializableResource.new(blogs, each_serializer: BlogSerializer),
+  #     meta: {
+  #       current_page: blogs.current_page,
+  #       total_pages: blogs.total_pages,
+  #       total_entries: blogs.total_count
+  #     }
+  #   }
+  # end
+
+
   def index
-    blogs = Blog.includes(:author).order(created_at: :desc).page(params[:page]).per(9)
+    per_page = params[:per_page] || 9 # Default to 9 if `per_page` is not provided
+  
+    # Filter blogs by status if provided, otherwise retrieve all blogs
+    if params[:status].present?
+      blogs = Blog.where(status: params[:status]).includes(:author).order(created_at: :desc).page(params[:page]).per(per_page)
+    else
+      blogs = Blog.includes(:author).order(created_at: :desc).page(params[:page]).per(per_page)
+    end
     
     render json: {
       blogs: ActiveModelSerializers::SerializableResource.new(blogs, each_serializer: BlogSerializer),
@@ -14,11 +38,12 @@ class  Api::V1::BlogsController < ApplicationController
       }
     }
   end
+  
 
   # GET /blogs/1
   def show
     @blog = Blog.find(params[:id])
-    render json: @blog, serializer: BlogSerializer, show_html: true
+    render json: @blog, serializer: BlogSerializer
   end
 
   # POST /blogs
@@ -29,6 +54,8 @@ class  Api::V1::BlogsController < ApplicationController
       blog_image = params[:blog][:blog_image]
       @blog.blog_image.attach(blog_image)
     end
+    @blog.status = params[:blog][:status] || 'draft'
+
     if @blog.save
       render json: @blog, status: :created
     else
@@ -45,21 +72,37 @@ class  Api::V1::BlogsController < ApplicationController
     end
   end
 
-  # DELETE /blogs/1
   def destroy
-    @blog.destroy!
+    @blog = Blog.find_by(id: params[:id], author_id: params[:author_id])
+  
+    if @blog
+      @blog.destroy!
+      render json: { message: 'Blog deleted successfully' }, status: :ok
+    else
+      render json: { error: 'Blog not found' }, status: :not_found
+    end
   end
+
+  def counts
+    published_count = Blog.published.count
+    draft_count = Blog.draft.count
+    
+    render json: { published_count: published_count, draft_count: draft_count }
+  end
+  
 
   private
 
-  
-    # def set_blog
-    #   # @blog = @author.blogs.find(params[:id])
 
-    # end
+  def set_blog
+    @blog = Blog.find_by(id: params[:id])
+    render json: { error: 'Blog not found' }, status: :not_found unless @blog
+  end
+
 
     # Only allow a list of trusted parameters through.
+   
     def blog_params
-      params.require(:blog).permit(:title, :content, :blog_image, :is_important, :author_id, :created_at)
+      params.require(:blog).permit(:title, :content, :excerpt, :blog_image, :is_important, :author_id, :status)
     end
 end
